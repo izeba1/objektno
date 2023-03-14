361797c6ebbf7a5370a788c7866a7672e8011284
Ivan Zeba <izeba1@tvz.hr>
2023-03-14 20:03:18 +0100

    prva vjezba

---
+++ main.py
@@ -0,0 +1,19 @@
+# Ova linija mora biti komentar!
+print('Dobrodošli', '!'*3)
+
+# Napisati sljedeće izraze u jednoj liniji koda
+print('Unesite vaše ime: ')
+ime = input()
+
+print('Unesite vaše prezime: ')
+prezime = input()
+ 
+print('Unesite vaš JMBAG: ')
+JMBAG = input()
+ 
+print('Unesite broj godina: ')
+godine = input()
+# TODO zatražite od korisnika broj godina
+
+# TODO ime i prezime ispišite u navodnim znakovima
+print("Korisnik : ", "'", ime, "'" " ", "'", prezime, "'", "", godine, end='!')
